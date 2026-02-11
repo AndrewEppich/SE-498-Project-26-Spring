df1b3009ec3b5a2943d2214ae8ffbe869054b236
Tom <red.shirt083@passinbox.com>
2026-02-10 22:49:17 -0800

    Adding db ensure created

--- src/Project498.WebApi/Program.cs
+++ src/Project498.WebApi/Program.cs
@@ -16,6 +16,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.EnsureCreatedAsync();
+    
     app.MapOpenApi();
     app.UseSwaggerUI(options =>
     {
