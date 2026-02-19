d24edb1696dc45f915ebb8c390dfdc17edf281eb
AndrewEppich <143771431+AndrewEppich@users.noreply.github.com>
2026-02-18 23:28:45 -0800

    Update StringService.cs

--- src/Project498.WebApi/Services/StringService.cs
+++ src/Project498.WebApi/Services/StringService.cs
@@ -5,7 +5,9 @@ public class StringService : IStringService
     public string Reverse(string input)
     {
         if (string.IsNullOrEmpty(input))
+        {
             return string.Empty;
+        }
 
         char[] charArray = input.ToCharArray();
         Array.Reverse(charArray);
